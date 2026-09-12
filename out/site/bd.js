bd9d543c23cbc765cfe2304d81d162c6487d862f
STG <168221516+CaveGameDev@users.noreply.github.com>
2026-09-12 09:29:55 -0700

    Delete out directory

--- out/site/bd.js
+++
@@ -1,9 +0,0 @@
-        (function() {
-            if (window.__resumeBoot) return;
-            var realBoot = window.onload;
-            window.onload = function() {};
-            window.__resumeBoot = function() {
-                window.__resumeBoot = function() {};
-                if (typeof realBoot === 'function') realBoot();
-            };
-        })();
